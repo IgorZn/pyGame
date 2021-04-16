d12b2f77444ca58b5096b00a98ea8eb9145090b0
Igor Znamensky <igor.znamensky@gmail.com>
2021-04-16 14:59:41 +0300

    Глава 12 • Инопланетное вторжение Next: Глава 13 • Осторожно, пришельцы!

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,14 +24,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Удаление снарядов, вышедших за край экрана
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -87,6 +80,19 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды."""
+        # Обновление позиций снарядов
+        self.bullets.update()
+
+        # Удаление снарядов, вышедших за край экрана
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
+
+
 
 
 if __name__ == '__main__':
