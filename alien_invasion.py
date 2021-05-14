56ea5bd48fbf5266050fe3399c26828802d13269
Igor Znamensky <igor.znamensky@gmail.com>
2021-05-14 10:53:56 +0300

    Глава 13 • Осторожно, пришельцы! Next: УПРАЖНЕНИЯ 13.3.

--- alien_invasion.py
+++ alien_invasion.py
@@ -101,7 +101,21 @@ class AlienInvasion:
 
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев во флоте."""
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _create_fleet(self):
         """Создание флота вторжения."""
